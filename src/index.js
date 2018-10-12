8b8f0ab03e65887347dd68a3425533848d6a29b4
shendan <664542051@qq.com>
2018-10-12 18:40:57 +0800

    3.3 编译ES6 增加babel-polyfill

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill'
 import sum from './sum.js'
 
 console.log('sum(23, 24)=', sum(23, 24))
@@ -5,3 +6,7 @@ console.log('sum(23, 24)=', sum(23, 24))
 let func = () => {};
 const Num = 45;
 let arr = [1, 2, 4];
+let arrB = arr.map(item => item * 2);
+
+console.log(arrB.includes(8));
+console.log('new Set(arrB) is ', new Set(arrB));
