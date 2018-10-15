fcec5c0cee0ba69f38f3c9285cf2ac205ded0a5b
shendan <664542051@qq.com>
2018-10-15 21:21:33 +0800

    3.4 提取公共代码

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,52 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        index: './src/index.js'
+        pageA: './src/pageA.js',
+        pageB: './src/pageB.js'
     },
     output: {
         filename: '[name].bundle.js',
         path: path.join(__dirname, 'dist')
     },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['@babel/preset-env', {
-                            targets: {
-                                browsers: ['> 1%', 'last 2 versions']
-                            }
-                        }]
-                    ],
-                    plugins: [require('@babel/plugin-transform-runtime')]
-                }
-            }
-        }]
+    optimization: {
+        splitChunks: {
+            chunks: 'async',
+            minSize: 1,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {}
+        }
     }
 }
+
+// module.exports = {
+//     entry: {
+//         index: './src/index.js'
+//     },
+//     output: {
+//         filename: '[name].bundle.js',
+//         path: path.join(__dirname, 'dist')
+//     },
+//     module: {
+//         rules: [{
+//             test: /\.js$/,
+//             exclude: /(node_modules)/,
+//             use: {
+//                 loader: 'babel-loader',
+//                 options: {
+//                     presets: [
+//                         ['@babel/preset-env', {
+//                             targets: {
+//                                 browsers: ['> 1%', 'last 2 versions']
+//                             }
+//                         }]
+//                     ],
+//                     plugins: [require('@babel/plugin-transform-runtime')]
+//                 }
+//             }
+//         }]
+//     }
+// }
