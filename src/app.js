ff22d0a7d1aa46803ff3a6f6fc1dff28c2735c43
tcshendan <664542051@qq.com>
2018-10-20 14:04:51 +0800

    3.6 处理css style-loader/useable

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-import "./css/base.css"
+//import "./css/base.css"
+import base from './css/base.css'
+
+var flag = false
+
+setInterval(function() {
+    if (flag) {
+        base.unuse()
+    } else {
+        base.use()
+    }
+    flag = !flag;
+}, 500)
